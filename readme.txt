9bcd37a0547cc7ef8b7abe9f82d4b6ed694af9f8
kewen <957314724@qq.com>
2017-05-05 14:59:33 +0800

    init

---
+++ readme.txt
@@ -0,0 +1,2 @@
+https://github.com/theJian/build-a-hn-front-page
+http://www.cnblogs.com/fengnovo/default.html?page=1
---
+++ src/js/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/App.css';
+
+class App extends React.Component {
+    constructor (...args) {
+        super(...args);
+    }
+
+    render() {
+        return <div className="font-c">kewen520
+        </div>;
+    }
+}
+
+export default App;
